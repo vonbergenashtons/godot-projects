71e8d4693bfec3cc39f8cdcd20888413a54d4dc6
Ashton Sean VonBergen <vonbergenashtons@gmail.com>
2024-11-13 09:20:38 -0700

    Added testing for commit and adding large astroid scene

--- astroids-challenge/spacefield.gd
+++ astroids-challenge/spacefield.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+@onready var astroid_large: CharacterBody2D = $AstroidLarge
+
 @export var large_astroid: PackedScene
 @export var medium_astroid: PackedScene
 @export var small_astroid: PackedScene
