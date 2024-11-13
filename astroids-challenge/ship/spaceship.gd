3444821d5cab22e415af992ce95b62812d258b7e
Ashton Sean VonBergen <vonbergenashtons@gmail.com>
2024-11-13 09:16:17 -0700

    Adding existing projects files in initial commit.

---
+++ astroids-challenge/ship/spaceship.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+signal hit
+
+@export var speed = 350
+@export var rotation_speed = 3.0
+
+var rotation_direction = 0
+
+func get_input():
+	rotation_direction = Input.get_axis("Rotate Left", "Rotate Right")
+	velocity = transform.x * Input.get_axis("Fly Backwards", "Fly Forward") * speed
+
+func _physics_process(delta):
+	get_input()
+	rotation += rotation_direction * rotation_speed * delta
+	var collision = move_and_collide(velocity * delta)
+	if collision:
+		print("Ship collided with ", collision.get_collider().name)
+	
+	
---
+++ astroids-challenge/spacefield.gd
@@ -0,0 +1,5 @@
+extends Node2D
+
+@export var large_astroid: PackedScene
+@export var medium_astroid: PackedScene
+@export var small_astroid: PackedScene
